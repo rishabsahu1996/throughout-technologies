0a3e52b4b3566867cc69898bfe679e66c5a73f2f
tout21 <spkumar7786@gmail.com>
2018-08-23 14:50:37 +0530

    Add Grous Table

--- WhatsappDB.sql
+++ WhatsappDB.sql
@@ -26,47 +26,24 @@ create database Whatapps;
 		Message varchar(1000),
 		MessageStatus varchar(20),
 		userId int , --foreign key
-		senderId int , --foreign key
+		OtherUserId int , --foreign key
 		MessageTime varchar(10),
 				primary key (MessageId),
 				FOREIGN KEY (userId) REFERENCES Users(userId),
-				FOREIGN KEY (senderId) REFERENCES Users(userId)				
+				FOREIGN KEY (OtherUserId) REFERENCES Users(userId)				
 		);
 
-	create table Groups (
-		GroupID int(20), -- primary key
-		GroupName varchar(20),
-		GroupCreatorID int,
-		GroupPPic varchar(20),
-		userId int , --foreign 
-				primary key (GroupID),
-				FOREIGN KEY (userId) REFERENCES Users(userId),
-				FOREIGN KEY (GroupCreatorId) REFERENCES Users(userId)				
-		);	
-
-	create table GroupDetails(
-		GDetailsID int(20), -- primary key
-		GroupID int;        --foreign 
-		MemberID varchar(20), --foreign 
-		AdminStatus varchar(20),
-		userId int ,         --foreign 
-				primary key (GDetailsID),
-				FOREIGN KEY (userId) REFERENCES Users(userId),
-				FOREIGN KEY (MemberId) REFERENCES Users(userId),
-				FOREIGN KEY (GroupID) REFERENCES Groups(GroupID)			
-		);	
 
 
 	create table ChatRooms (
 		ChatRoomId int  -- primary key
 		MessageID int(20), --foreign key
 		userId int , --foreign key 
-		FriendListId int, --foreign key
 		ArchivedChats int ,
 		ReadStatus int ,
 				primary key (ChatRoomId),
 				FOREIGN KEY (userId) REFERENCES Users(userId),
 				FOREIGN KEY (MessageID) REFERENCES Messages(MessageID),
-				FOREIGN KEY (FriendListId) REFERENCES FriendLists(FriendListId)
+			
 		);
 	
